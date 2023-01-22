2dbff4d0f83ae2b8dc8707d1734d7fbffddf3159
kijiwon <119961147+kijiwon@users.noreply.github.com>
2023-01-22 21:55:39 +0900

    js 버튼 이벤트 오류 해결

--- 계산기/script.js
+++ 계산기/script.js
@@ -3,13 +3,18 @@ const display = document.querySelector('.output');
 const calculator = document.querySelector('.container');
 const buttons = calculator.querySelector('.cal-btns');
 const switch_mode = document.querySelector('.switch-mode');
-const start_btn = document.querySelector('#start-cal');
-const close_btn = document.querySelector('.close-btn');
-close_btn.onclick = function(){
-    calculator.classList.add('.hide');
-    start_btn.classList.remove('hide');
+const start_btn = document.getElementById('start-cal')
+const close_btn = document.getElementById('close-btn')
+const startBox = document.querySelector('.start');
+// 왜 동작을 안해,,ㅜ -> .을 넣지 말아야지!
+// 버튼 이벤트 넣기
+function startCal (){
+    startBox.classList.add('hide');
+    calculator.classList.remove('hide');
 }
-start_btn.onclick = function(){
-    start_btn.classList.add('.hide');
-    calculator.classList.remove('.hide');
+function closeCal(){
+    startBox.classList.remove('hide');
+    calculator.classList.add('hide');
 }
+start_btn.addEventListener('click',startCal);
+close_btn.addEventListener('click',closeCal);
