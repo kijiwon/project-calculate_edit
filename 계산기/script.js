d6e3e89f72590860c1a709b70eed4332c376226d
kijiwon <119961147+kijiwon@users.noreply.github.com>
2023-01-24 16:04:58 +0900

    js 구현 완료

--- 계산기/script.js
+++ 계산기/script.js
@@ -1,13 +1,15 @@
 const body = document.querySelector('body');
+// 계산 구현 
 const display = document.querySelector('.output');
 const calculator = document.querySelector('.container');
 const buttons = calculator.querySelector('.cal-btns');
+let firstNum, operator, previousKey, previousNum;
+// 시작/닫기 이벤트구현
 const switch_mode = document.querySelector('.switch-mode');
 const start_btn = document.getElementById('start-cal')
 const close_btn = document.getElementById('close-btn')
 const startBox = document.querySelector('.start');
-let firstNum, operator, previousKey, previousNum;
-// 왜 동작을 안해,,ㅜ -> .을 넣지 말아야지!
+
 // 버튼 이벤트 넣기
 function startCal (){
     startBox.classList.add('hide');
@@ -19,7 +21,6 @@ function closeCal(){
 }
 start_btn.addEventListener('click',startCal);
 close_btn.addEventListener('click',closeCal);
-
 // 다크모드 / 라이트모드
 switch_mode.onclick = function(){
     body.classList.toggle('dark');
@@ -42,8 +43,10 @@ function calculate(n1, operator, n2){
     if(operator === '^'){
         result = Math.pow(Number(n1),Number(n2));
     }
+    // n1은 n2의 몇 퍼센트인지 계산
     if(operator === '%'){
-        result = Number(n1)/100;
+        result = (Number(n1)/Number(n2))*100;
+        result = `${result}%`
     }
     return String(result);
 }
@@ -53,6 +56,56 @@ buttons.addEventListener('click', function (event) {
     const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보
     const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보
     const buttonContainerArray = buttons.children;
-    console.dir(event);
-
+    // console.dir(event);
+    if(target.matches('button')){
+        for(let i=0; i<buttonContainerArray.length; i++){
+            const childrenArray = buttonContainerArray[i].children;
+            for(let j=0; j<childrenArray.length; j++){
+                childrenArray[j].classList.remove('isPressed');
+            }
+        }
+        if(action === 'number'){
+            if(display.textContent==='0' || previousKey === 'operator' || previousKey === 'calculate'){
+                display.textContent = buttonContent;
+            } else {
+                display.textContent = display.textContent + buttonContent;
+            }
+            previousKey = 'number';
+        }
+        if (action === 'oper') {
+            target.classList.add('isPressed');
+            if (firstNum && operator && previousKey !== 'operator' && previousKey !== 'calculate') {
+              display.textContent = calculate(firstNum, operator, display.textContent);
+            }
+            firstNum = display.textContent;
+            operator = buttonContent;
+            previousKey = 'operator';
+        }
+        if (action === 'decimal') {
+            if (!display.textContent.includes('.') && previousKey !== 'operator') {
+              display.textContent = display.textContent + '.';
+            } else if (previousKey === 'operator') {
+              display.textContent = '0.';
+            }
+            previousKey = 'decimal';
+        }
+        if (action === 'clear') {
+            firstNum = undefined;
+            operatorForAdvanced = undefined;
+            previousNum = undefined;
+            previousKey = 'clear';
+            display.textContent = '0';
+        }
+        if (action === 'cal') {
+            if (firstNum) {
+              if (previousKey === 'calculate') {
+                display.textContent = calculate(display.textContent, operator, previousNum);
+              } else {
+                previousNum = display.textContent;
+                display.textContent = calculate(firstNum, operator, display.textContent);
+              }
+            }
+            previousKey = 'calculate';
+        }
+    }
 });
